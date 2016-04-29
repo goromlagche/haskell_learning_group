08180f25de794fc8cda7bb8eb5b508be3ccd829d
goromlagche <mrinmoy.das91@gmail.com>
2016-04-30 03:30:01 +0530

    add solution to ex-5.4

--- ex-5-cs-194/Cal.hs
+++ ex-5-cs-194/Cal.hs
@@ -20,3 +20,26 @@ instance Expr ExprT where
     lit = Lit
     add = Add
     mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit                       = MinMax
+  add (MinMax a) (MinMax b) = lit $ max a b
+  mul (MinMax a) (MinMax b) = lit $ min a b
+
+instance Expr Mod7 where
+  lit n                 = Mod7 $ n `mod` 7
+  add (Mod7 a) (Mod7 b) = lit $ a + b
+  mul (Mod7 a) (Mod7 b) = lit $ a * b
