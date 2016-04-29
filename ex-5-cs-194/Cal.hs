01cf7129f161a822f95fcf0ed53fe2632dbd188a
goromlagche <mrinmoy.das91@gmail.com>
2016-04-30 00:36:35 +0530

    add solution to ex-5.2

--- ex-5-cs-194/Cal.hs
+++ ex-5-cs-194/Cal.hs
@@ -1,9 +1,12 @@
 module Cal where
 
 import ExprT
-import Test.QuickCheck
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit a)   = a
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
+
+evalStr :: String -> Maybe Integer
+evalStr s  = fmap eval $ parseExp Lit Add Mul s
--- ex-5-cs-194/CalTest.hs
+++ ex-5-cs-194/CalTest.hs
@@ -1,17 +1,35 @@
 module CalTest where
-import ExprT
-import Cal
+
 import Test.Hspec
 import Test.QuickCheck
+import Data.Monoid
+
+import ExprT
+import Parser
+import Cal
 
 main :: IO ()
 main = hspec $ do
-  describe "eval" $ do
-    it "passes the the spec mentioned in the exercise" $ do
-      eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
-    it "adds two integers" $ do
-      property $ \x y -> eval (Add (Lit x) (Lit y)) == x + y
-    it "multiplies two integers" $ do
-      property $ \x y -> eval (Mul (Lit x) (Lit y)) == x * y
-    it "add and then multiplies" $ do
-      property $ \x y z -> eval (Mul (Add (Lit x) (Lit y)) (Lit z)) == (x + y) * z
+  describe "Cal" $ do
+    describe "eval" $ do
+      it "passes the the spec mentioned in the exercise" $ do
+        eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+      it "adds two integers" $ do
+        property $ \x y -> eval (Add (Lit x) (Lit y)) == x + y
+      it "multiplies two integers" $ do
+        property $ \x y -> eval (Mul (Lit x) (Lit y)) == x * y
+      it "add and then multiplies" $ do
+        property $ \x y z ->
+          eval (Mul (Add (Lit x) (Lit y)) (Lit z)) == (x + y) * z
+
+    describe "evalStr" $ do
+      it "passes the the spec mentioned in the exercise" $ do
+        evalStr "(2+3)*4" `shouldBe` Just 20
+      it "does addition" $ do
+        property $ \x y -> evalStr (show x <> "+" <> show y) == Just (x + y)
+      it "does multiplication" $ do
+        property $ \x y -> evalStr (show x <> "*" <> show y) == Just (x * y)
+      it "adds and then multiplies" $ do
+        property $ \x y z ->
+          evalStr ("(" <> show x <> "+" <> show y <> ")" <> "*" <> show z) ==
+          Just ((x + y) * z)
