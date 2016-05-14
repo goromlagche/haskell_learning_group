5ff6b724624ed03328f9c36e85a17598bba80955
goromlagche <mrinmoy.das91@gmail.com>
2016-05-15 01:23:44 +0530

    add sol to ex-2-2

--- monad_challenges/Set2.hs
+++ monad_challenges/Set2.hs
@@ -1,9 +1,44 @@
-module Set2 (mkSeed) where
+module Set2 (mkSeed
+            , MyMaybe(..)
+            , headMay
+            , tailMay
+            , lookupMay
+            , divMay
+            , maximumMay
+            , minimumMay) where
 
 import MCPrelude
 
-data MyMaybe a = MyNothing | MyJust a
+data MyMaybe a = MyNothing | MyJust a deriving (Eq)
 
 instance Show a => Show (MyMaybe a) where
-  show MyNothing = "MyNothing"
+  show MyNothing  = "MyNothing"
   show (MyJust a) = "MyJust " ++ show a
+
+headMay :: [a] -> MyMaybe a
+headMay []     = MyNothing
+headMay (a:_) = MyJust a
+
+tailMay :: [a] -> MyMaybe [a]
+tailMay []     = MyNothing
+tailMay (_:as) = MyJust as
+
+lookupMay :: Eq a => a -> [(a, b)] -> MyMaybe b
+--  lookup already does this :-/
+lookupMay _ [] = MyNothing
+lookupMay a ((x,y):xs)
+  | a == x    = MyJust y
+  | otherwise = lookupMay a xs
+
+
+divMay :: (Eq a, Fractional a) => a -> a -> MyMaybe a
+divMay _ 0   = MyNothing
+divMay a1 a2 = MyJust (a1 / a2)
+
+maximumMay :: Ord a => [a] -> MyMaybe a
+maximumMay [] = MyNothing
+maximumMay n  = MyJust (maximum n)
+
+minimumMay :: Ord a => [a] -> MyMaybe a
+minimumMay [] = MyNothing
+minimumMay n  = MyJust (minimum n)
--- monad_challenges/Set2Test.hs
+++ monad_challenges/Set2Test.hs
@@ -1,7 +1,16 @@
 module Set2Test where
 
-import Set2
 import Test.Hspec
+import Test.QuickCheck
 
+import Set2
+
+-- getting some error which I can't debug :(
 -- main :: IO()
 -- main = hspec $ do
+--   describe "headMay" $ do
+--     describe "provides safe head" $ do
+--       it "returns Nothing for empty list" $
+--         headMay([]) `shouldBe` MyNothing
+--       it "returns Head for list" $
+--         property $ \[x] -> headMay [x] == MyJust (head [x])
